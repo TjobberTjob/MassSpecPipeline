0e8339945ecd02dff0c6e4695e0bbf15e3b96610
TjobberTjob <tobiasgchristensen@outlook.dk>
2019-09-30 10:08:31 +0200

    updated

--- movetofolder.py
+++ movetofolder.py
@@ -1,6 +1,6 @@
 import os 
 import glob
-datapath = "data/ProteomeToolsRaw/"
+datapath = "/data/ProteomeToolsRaw/"
 for files in [f for f in glob.glob(datapath + "**/*.raw", recursive=True)]:
 	if not os.path.exists(files[:-4]+"/"):
 		os.mkdir(files[:-4]+"/")
