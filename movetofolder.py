8c46dcfbeed069a07ac883c8ca77b7dd9168555f
TjobberTjob <tobiasgchristensen@outlook.dk>
2019-09-30 10:04:34 +0200

    updated this

--- movetofolder.py
+++ movetofolder.py
@@ -1,7 +1,12 @@
 import os 
 import glob
 datapath = "data/ProteomeToolsRaw/"
-for files in [f for f in glob.glob(datapath + "**/*", recursive=True)]:
+for files in [f for f in glob.glob(datapath + "**/*.raw", recursive=True)]:
+	if not os.path.exists(files[:-4]+"/"):
+		os.mkdir(files[:-4]+"/")
+	os.system("mv "+files+" "+files[:-4])
+
+for files in [f for f in glob.glob(datapath + "**/*.zip", recursive=True)]:
 	if not os.path.exists(files[:-4]+"/"):
 		os.mkdir(files[:-4]+"/")
 	os.system("mv "+files+" "+files[:-4])
