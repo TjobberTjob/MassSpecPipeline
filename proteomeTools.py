361ede712e92fbfd0fbe038b7f7c0d2d9407871e
TjobberTjob <tobiasgchristensen@outlook.dk>
2019-10-02 13:36:29 +0200

    updated this

--- proteomeTools.py
+++ proteomeTools.py
@@ -111,7 +111,7 @@ def internalmzML(filename):
 
 		stats = {'ms-levels':{1:0}}
 
-		for spectrum in data:
+		for spectrum in data[1000:]:
 			# print(str(scan_id = int(spectrum['id'].split('scan=')[1])))
 			max_spec = spectrum['id'].split('scan=')[1]
 			print(max_spec)
