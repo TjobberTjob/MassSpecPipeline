0ae8e9cc9e5c06e5b08ba986f3f89beb6536889b
Tjobbertjob <tobiasgchristensen@outlook.dk>
2019-10-08 13:31:09 +0200

    updated and added

--- filecheck.py
+++ filecheck.py
@@ -1,8 +1,9 @@
 i = 0
+datapath = "/data/ProteomeToolsRaw/Images/"
 for line in open(datapath+'metadata.json'):
 	data = json.loads(line)
 	imname = data['image']
-	if not os.path.exists("/data/ProteomeToolsRaw/Images/"+imname):
+	if not os.path.exists(datapath+imname):
 		i = i+1
 		print("doesnt exist")
 print(i)
