c4230f635a123e3e44ef1bfe759292a73d9aad44
root <root@DESKTOP-TE5U5Q4.localdomain>
2019-10-24 01:46:44 +0200

    updated

--- tryptic.py
+++ tryptic.py
@@ -1,4 +1,4 @@
-datapath = "/data/ProteomeToolsRaw/"
+datapath = "/data/ProteomeToolsRaw/tryptic/"
 if __name__ == '__main__':
 	import shutil
 	import pandas as pd
@@ -22,8 +22,31 @@ if __name__ == '__main__':
 files = [os.path.dirname(p) for p in glob.glob(datapath+"/*/*")]
 files = np.unique(files)
 for f in files:
-	file = f[23:]+".raw"
-	if os.path.exists(f+"/"+file):
-		shutil.move(f+"/"+file, f+"/file.raw")
-	else:
-		continue
+	file = f[31:]+".zip"
+	if os.path.exists(f+'/'+file):
+		print(file)
+
+
+
+#files = [os.path.dirname(p) for p in glob.glob(datapath+"/*/*")]
+#files = np.unique(files)
+#for f in files:
+#	file = f[31:]+".raw"
+#	if os.path.exists(f+file):
+#		os.system(f+file+" "+2)
+#	subprocess.run('unzip -j '+f+"/"+file+' allPeptides.txt -d '+f,shell = True)
+#
+#files = np.unique(files)
+#
+#for f in files:
+#	file = f[31:]+".zip"
+#	if f+file == '/data/ProteomeToolsRaw/tryptic/TUM_first_pool_97_01_01_2xIT_2xHCD-1h-R2-tryptic':
+#		ss = ','
+#	else:
+#		ss = '\t'
+#	df = pd.read_csv(f+'/allPeptides.txt', sep = ss)
+#	print('/data/ProteomeToolsRaw/01650b_BG7-TUM_first_pool_97_01_01-2xIT_2xHCD-1h-R2/file.zip')
+#	try:
+#		shutil.move(f+'/'+file, '/data/ProteomeToolsRaw/'+df.iloc[0,0]+'/file.zip')
+#	except:
+#		print("moved")
