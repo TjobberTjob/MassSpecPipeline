264eff3cc366a3ab98ea5359a8c685d2a3b702c8
root <root@DESKTOP-TE5U5Q4.localdomain>
2019-10-23 23:19:54 +0200

    updated

--- tryptic.py
+++ tryptic.py
@@ -7,4 +7,4 @@ files = [os.path.dirname(p) for p in glob.glob(datapath+"/*/*")]
 
 for f in files:
 	file = f[31:]+".zip"
-	subprocess.run('unzip -j '+f+"/"+file+' txt/allPeptides.txt -d '+f,shell = True)
+	subprocess.run('unzip -j '+f+"/"+file+' allPeptides.txt -d '+f,shell = True)
