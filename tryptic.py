4e8e51b97f3cbbf9c264935021a8ac11c8eebd86
root <root@DESKTOP-TE5U5Q4.localdomain>
2019-10-23 23:13:30 +0200

    updated

--- tryptic.py
+++ tryptic.py
@@ -1,5 +1,8 @@
 datapath = "/data/ProteomeToolsRaw/tryptic/"
 import os 
 import glob
+import re
 files = [os.path.dirname(p) for p in glob.glob(datapath+"/*/*")]
 print(files)
+for f in files:
+	print(re.match("/", f).end())
