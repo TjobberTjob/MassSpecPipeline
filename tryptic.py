fa831a00e211e548f7a57b7a79b295149dcd77d3
root <root@DESKTOP-TE5U5Q4.localdomain>
2019-10-23 23:52:32 +0200

    updated

--- tryptic.py
+++ tryptic.py
@@ -12,12 +12,7 @@ for f in files:
 		continue
 	subprocess.run('unzip -j '+f+"/"+file+' allPeptides.txt -d '+f,shell = True)
 
-i=1
 for f in files:
-	if i > 1:
-		quit()
 	df = pd.read_csv(f+'/allPeptides.txt', sep = ',')
-	print(df.head())
-	i=i+1
-	#df2 = df.loc[df['Sequence'] != ' ',]
-	#pd.DataFrame.to_csv(df2,f+'/allPeptides.txt')
+	df2 = df.loc[df['Sequence'] != ' ',]
+	pd.DataFrame.to_csv(df2,f+'/allPeptides.txt')
