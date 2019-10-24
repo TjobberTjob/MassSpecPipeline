483499dd2e269970a1e5893209e0c5d642bf2fdc
root <root@DESKTOP-TE5U5Q4.localdomain>
2019-10-25 00:14:29 +0200

    updated

--- classes.py
+++ classes.py
@@ -29,8 +29,8 @@ if len(udirs) != 0:
 	if reset == "yes" or reset == "y":
 		for files in udirs:
 			os.system('find '+str(files)+' -name \"*.png\" -exec mv -t '+ str(datapath)+ ' {} +')
-			os.system("rm -rf "+trainpath)
-			os.system("rm -rf "+valpath)
+		os.system("rm -rf "+trainpath)
+		os.system("rm -rf "+valpath)
 
 			##ALTERANTIVES###
 			#shutil.move(images,datapath)
