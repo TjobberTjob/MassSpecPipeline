ed05fd9062f4e4940846d0e6d6c9cf7d771533bf
root <root@DESKTOP-TE5U5Q4.localdomain>
2019-10-24 22:12:57 +0200

    updated

--- tryptic.py
+++ tryptic.py
@@ -32,5 +32,3 @@ for f in files:
 	#if not os.path.exists(datapath+name+'/file.zip'):
 		#shutil.copyfile(f, datapath+name+'/file.zip')
 	os.system('rm '+datapath+'allPeptides.txt')
-
-cp /data/ProteomeToolsRaw/TUM_first_pool_1_01_01_2xIT_2xHCD-1h-R1-tryptic.zip /data/ProteomeToolsRaw/01625b_GA1-TUM_first_pool_1_01_01-2xIT_2xHCD-1h-R1/file.zip
