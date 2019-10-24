aa31862347b4f9f239976b33901a5a4321edea01
root <root@DESKTOP-TE5U5Q4.localdomain>
2019-10-24 16:34:15 +0200

    updated

--- tryptic.py
+++ tryptic.py
@@ -23,10 +23,10 @@ if __name__ == '__main__':
 files = glob.glob(datapath+"*.zip")
 files = np.unique(files)
 for f in files:
-	#subprocess.run('unzip -j '+f+' allPeptides.txt -d '+datapath,shell = True)
+	subprocess.run('unzip -j '+f+' allPeptides.txt -d '+datapath,shell = True)
 	
-	with ZipFile(f, 'r') as zipObj:
-		zipObj.extract(allPeptides.txt, 'allPeptides.txt')
+	#with ZipFile(f, 'r') as zipObj:
+	#	zipObj.extract(allPeptides.txt, 'allPeptides.txt')
 	df = pd.read_csv(datapath+'allPeptides.txt', sep = "\t")
 	df2 = pd.read_csv(datapath+'allPeptides.txt', sep = ",")
 	print(df.iloc[0,0])
@@ -34,5 +34,5 @@ for f in files:
 	#if not os.path.exists(datapath+df.iloc[0,0]+'/file.zip'):
 	#	print(os.path.exists(datapath+df.iloc[0,0]+'/file.zip')+ df.iloc[0,0])
 	#	#shutil.move(f, datapath+df.iloc[0,0]+'/file.zip')
-	#os.system('rm '+datapath+'allPeptides.txt')
+	os.system('rm '+datapath+'allPeptides.txt')
 
