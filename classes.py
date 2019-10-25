30356a929dbae6660bc02056a8aec430d3359503
root <root@DESKTOP-TE5U5Q4.localdomain>
2019-10-25 12:18:50 +0200

    updated´

--- classes.py
+++ classes.py
@@ -70,8 +70,7 @@ def classifyImages(classes):
 		for k, v in imgdata.items():
 			imgadata[v].append(k)
 		for f in imgadata:
-			print("mv "+datapath+str(imgadata[f])+" "+trainpath+f+"/"+str(imgadata[f]))
-			#shutil.move(datapath+str(imgadata[f]), trainpath+f+"/"+str(imgadata[f]))
+			shutil.move(datapath+str(imgadata[f]), trainpath+f+"/"+str(imgadata[f]))
 		quit()
 		#CREATING VALIDATION DATA
 		print("Sorting into Validation data")
