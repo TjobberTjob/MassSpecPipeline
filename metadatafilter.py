b74874b82c2fff9b6757e225241913c229bb0f06
TjobberTjob <tobiasgchristensen@outlook.dk>
2019-11-04 12:29:20 +0100

    updated this

--- metadatafilter.py
+++ metadatafilter.py
@@ -11,9 +11,8 @@ lines_seen = set()
 outfile = open(path+'metadata_filtered.json','w')
 for line in open(path+'metadata.json','r'):
 	data = json.loads(line)
-	print(float(data['m/z']))
 
-	if float(data['m/z']) > 429.5 and float(data['m/z']) < 425: #Add filter here
+	if float(data['m/z']) > 424.5 and float(data['m/z']) < 425: #Add filter here
 		if line not in lines_seen:
 			outfile.write(line)
 			lines_seen.add(line)
