9e021a1d40e2cf233983f6112855de5aa1b34a95
TjobberTjob <tobiasgchristensen@outlook.dk>
2019-11-04 11:54:33 +0100

    updated this

--- metadatacleaner.py
+++ metadatacleaner.py
@@ -10,9 +10,8 @@ except Exception:
 lines_seen = set()
 outfile = open(path+'metadata_cleaned.json','w')
 for line in open(path+'metadata.json','r'):
-	print(line["m/z"])
-	quit()
-	if float(line['m/z']) > 429.5 and float(line['m/z']) < 425: #Add filter here
+	data = json.loads(line)
+	if float(data['m/z']) > 429.5 and float(data['m/z']) < 425: #Add filter here
 		if line not in lines_seen:
 			outfile.write(line)
 			lines_seen.add(line)
