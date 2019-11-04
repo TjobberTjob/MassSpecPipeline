4c71a8650446f11a8a6d6eb8e58f47290e67b111
TjobberTjob <tobiasgchristensen@outlook.dk>
2019-11-04 11:48:31 +0100

    updated this

--- metadatacleaner.py
+++ metadatacleaner.py
@@ -3,14 +3,14 @@ import os
 
 path = '/data/ProteomeToolsRaw/Images/'
 try:
-	os.system('rm '+path+"metadata_cleaned.json")
+	os.system('rm '+path+'metadata_cleaned.json')
 except Exception:
-	print("no cleaned version exist")
+	print('no cleaned version exist')
 
 lines_seen = set()
-outfile = open(path+"metadata_cleaned.json","w")
-for line in open(path+"metadata.json","r"):
-	if float(line['m/z']) > int(424.95) and float(line['m/z']) < int(425):
+outfile = open(path+'metadata_cleaned.json','w')
+for line in open(path+'metadata.json','r'):
+	if float(line['m/z']) > 429.5 and float(line['m/z']) < 425: #Add filter here
 		if line not in lines_seen:
 			outfile.write(line)
 			lines_seen.add(line)
