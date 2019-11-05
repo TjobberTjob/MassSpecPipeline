e3a6c78db0d420fce74c16707656822fc7c6020e
TjobberTjob <tobiasgchristensen@outlook.dk>
2019-11-05 11:55:24 +0100

    updated this

--- prideDL.py
+++ prideDL.py
@@ -384,7 +384,7 @@ if __name__ == '__main__':
 
 	if len(rawurls) == len(zipurls): 
 		for f in rawurls:
-			filename = f[63:-4]
+			filename = f[38:-4]
 			if filename == "01625b_GA1-TUM_first_pool_1_01_01-2xIT_2xHCD-1h-R1":
 				continue
 			print('\nfile: '+filename)
