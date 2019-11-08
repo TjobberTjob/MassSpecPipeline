7ccdc914f9b92f2a37cb8323dea7e68123248582
root <root@DESKTOP-TE5U5Q4.localdomain>
2019-11-08 17:24:24 +0100

    updated

--- prideDL.py
+++ prideDL.py
@@ -345,8 +345,8 @@ if __name__ == '__main__':
 	for zips in searchfiles:
 		print(zips)
 		os.system('wget -q --show-progress -O '+datapath+'/file.zip'+' -c '+zips)
-		with ZipFile.namelist('file.zip','r') as zipped:
-			ziplist = zipped.namelist() 
+		# with ZipFile.namelist('file.zip','r') as zipped:
+			# ziplist = zipped.namelist() 
 		for a in ziplist:
 			if pepfile in a:
 				subprocess.run('unzip -j '+datapath+'/file.zip '+a+' -d '+datapath+'/',shell = True)
