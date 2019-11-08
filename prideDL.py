831a26b4cae6ac71e2061bd7e35f09e2568b4c0f
root <root@DESKTOP-TE5U5Q4.localdomain>
2019-11-08 18:24:25 +0100

    updated

--- prideDL.py
+++ prideDL.py
@@ -348,10 +348,12 @@ if __name__ == '__main__':
 			ziplist = zipped.namelist()
 		for a in ziplist:
 			if pepfile in str(a):
-				subprocess.run('unzip -j '+datapath+'/file.zip '+a+' -d '+datapath+'/',shell = True)
+				subprocess.run('unzip -j '+datapath+'file.zip '+a+' -d '+datapath+'/',shell = True)
 				break
 		df2 = pd.read_csv(datapath+pepfile,sep='\t')
 		rawfiles = np.unique(df.iloc[:0])
+		print(rawfiles)
+		quit()
 		for raws in rawfiles:
 			filename = raws[:-4]
 			if not os.path.exists(datapath+filename+'/file.zip'):
