478e23ac674c113fc9e3900af30255a3f7735140
root <root@DESKTOP-TE5U5Q4.localdomain>
2019-11-27 22:43:22 +0100

    updated

--- prideDL.py
+++ prideDL.py
@@ -407,8 +407,8 @@ def combined(accession, maxquant_file, path, metapath):
 
 if __name__ == '__main__':
 	#Path to data
-	# datapath = '/data/ProteomeToolsRaw/' #Server datapath
-	datapath = 'Data/' 
+	datapath = '/data/ProteomeToolsRaw/' #Server datapath
+	# datapath = 'Data/' 
 	metapath = datapath+'metadata/'
 
 	#Assigning accession number and maxquant output file name
