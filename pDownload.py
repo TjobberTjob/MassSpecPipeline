9b0bc81c62bcee4c55ed6dab82b0e59fd2439571
root <root@DESKTOP-TE5U5Q4.localdomain>
2019-12-05 12:37:24 +0100

    updated

--- pDownload.py
+++ pDownload.py
@@ -443,7 +443,7 @@ if __name__ == '__main__':
 	#Assigning accession number and maxquant output file name
 	pepfile = 'allPeptides.txt'
 	if len(sys.argv[1] != 9):
-			for line in open(metapath+sys.argv[1]):
+			for line in open(metapath+sys.argv[1]+'.json'):
 				accession = str(line[15:24])
 				try:
 					combined(accession, maxquant_file = pepfile, path = datapath, metapath = metapath)
