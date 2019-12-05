bc538cce70a21738c782c8ab02386466b1dbc75c
root <root@DESKTOP-TE5U5Q4.localdomain>
2019-12-05 13:03:02 +0100

    updated

--- mdfilter.py
+++ mdfilter.py
@@ -15,7 +15,7 @@ def filter(path, file):
 		data = json.loads(line)
 		##### ADD FILTER HERE #####
 		try:
-			if data['allpeptides'] == 'True' and data['Modification'] != 'No PTMs are included in the dataset':
+			if data['allpeptides'] == 'True':# and data['Modification'] != 'No PTMs are included in the dataset':
 		###########################
 				if line not in lines_seen:
 					outfile.write(line)
