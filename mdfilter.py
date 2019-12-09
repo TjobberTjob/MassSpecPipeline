32ba8481636f39efc75a2cd3d046919859500715
root <root@DESKTOP-TE5U5Q4.localdomain>
2019-12-09 17:23:20 +0100

    updated

--- mdfilter.py
+++ mdfilter.py
@@ -14,14 +14,14 @@ def filter(path, file):
 	for line in open(path+str(filetofilter)+'.json','r'):
 		data = json.loads(line)
 		##### ADD FILTER HERE #####
-		try:
-			if data['allpeptides'] and data['allpeptides'] != 'Error' and 'raw' in data['filetypes']:# and data['Modification'] != 'No PTMs are included in the dataset':
+		# try:
+		# 	if data['allpeptides'] and data['allpeptides'] != 'Error' and 'raw' in data['filetypes']:# and data['Modification'] != 'No PTMs are included in the dataset':
 		###########################
-				if line not in lines_seen:
-					outfile.write(line)
-					lines_seen.add(line)
-		except Exception:
-			pass
+		if line not in lines_seen:
+			outfile.write(line)
+			lines_seen.add(line)
+		# except Exception:
+		# 	pass
 	outfile.close()
 
 
