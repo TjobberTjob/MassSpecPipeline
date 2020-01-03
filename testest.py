af9cf275c19a05cbfb8450bba18e680dc66d1a89
TobiaGC <TobiasGChristensen@outlook.dk>
2020-01-03 22:42:14 +0100

    `updated`

--- testest.py
+++ testest.py
@@ -5,11 +5,11 @@ with open('config.json') as json_file:
 path = data['path'] + 'metadata/'
 
 Seen = []
+lendict = {}
 i = 0
 for line in open(path + 'subimage.json'):
 	data = json.loads(line)
 	name = str(data['Sequence'])
-	lendict = {}
 	if name not in Seen:
 		Seen.append(name)
 		i += 1
