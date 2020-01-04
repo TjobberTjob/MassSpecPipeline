ac61e5172c2428e29ae3c1c691f9edb406354970
TobiaGC <TobiasGChristensen@outlook.dk>
2020-01-04 21:26:56 +0100

    `updated`

--- testest.py
+++ testest.py
@@ -9,15 +9,18 @@ Seen = []
 lendict = {}
 i = 0
 for line in open(path + 'subimage.json'):
-	data = json.loads(line)
-	name = str(data['Sequence'])
-	if name not in Seen:
-		Seen.append(name)
-		i += 1
 	try:
-		lendict[len(name)] = int(lendict[len(name)]) + 1
+		data = json.loads(line)
+		name = str(data['Sequence'])
+		if name not in Seen:
+			Seen.append(name)
+			i += 1
+		try:
+			lendict[len(name)] = int(lendict[len(name)]) + 1
+		except:
+			lendict[len(name)] = 1
 	except:
-		lendict[len(name)] = 1
+		pass
 print(Seen)
 print('files in '+str(i)+' different classes')
 print(lendict)
