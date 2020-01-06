32a2c29a86791dae9d17135bf94a8b3cb1f45a9a
TobiaGC <TobiasGChristensen@outlook.dk>
2020-01-07 00:05:34 +0100

    `updated`

--- testest.py
+++ testest.py
@@ -7,21 +7,19 @@ path = data['path'] + 'metadata/'
 
 Seen = []
 lendict = {}
+strdict = {}
 i = 0
 for line in open(path + 'subimage.json'):
 	try:
 		data = json.loads(line)
 		name = str(data['Sequence'])
-		if name not in Seen:
-			Seen.append(name)
-			i += 1
+		Seen.append(name)
 		try:
 			lendict[len(name)] = int(lendict[len(name)]) + 1
 		except:
 			lendict[len(name)] = 1
 	except:
 		pass
-
 a = {}
 for f in Seen:
 	a[str(f)] = Seen.count(f)
