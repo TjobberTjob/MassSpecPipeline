4f406f647096cae0066dab3bc280aa637298d10e
TobiaGC <TobiasGChristensen@outlook.dk>
2020-01-06 13:57:22 +0100

    `updated`

--- testest.py
+++ testest.py
@@ -1,27 +1,26 @@
-# import json
-# import os
-os.listdir
+import json
+import os
 
-# with open('config.json') as json_file:
-# 	data = json.load(json_file)
-# path = data['path'] + 'metadata/'
-#
-# Seen = []
-# lendict = {}
-# i = 0
-# for line in open(path + 'subimage.json'):
-# 	try:
-# 		data = json.loads(line)
-# 		name = str(data['Sequence'])
-# 		if name not in Seen:
-# 			Seen.append(name)
-# 			i += 1
-# 		try:
-# 			lendict[len(name)] = int(lendict[len(name)]) + 1
-# 		except:
-# 			lendict[len(name)] = 1
-# 	except:
-# 		pass
-# print(Seen)
-# print('files in '+str(i)+' different classes')
-# print(lendict)
+with open('config.json') as json_file:
+	data = json.load(json_file)
+path = data['path'] + 'metadata/'
+
+Seen = []
+lendict = {}
+i = 0
+for line in open(path + 'subimage.json'):
+	try:
+		data = json.loads(line)
+		name = str(data['Sequence'])
+		if name not in Seen:
+			Seen.append(name)
+			i += 1
+		try:
+			lendict[len(name)] = int(lendict[len(name)]) + 1
+		except:
+			lendict[len(name)] = 1
+	except:
+		pass
+print(Seen)
+print('files in '+str(i)+' different classes')
+print(lendict)
