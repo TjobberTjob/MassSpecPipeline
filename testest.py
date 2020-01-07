aec5d4ffa35ff68df26a6ae9b71c3a8649ff343b
TobiaGC <TobiasGChristensen@outlook.dk>
2020-01-07 13:02:13 +0100

    `updated`

--- testest.py
+++ testest.py
@@ -8,20 +8,26 @@ path = data['path'] + 'metadata/'
 # names = [(json.loads(line)['image']+".txt") for line in open(path + 'subimage.json') if 'image' in json.loads(line)]
 Seen = []
 lendict = {}
+leng = []
 for line in open(path + 'subimage.json'):
 	try:
 		data = json.loads(line)
 		name = str(data['Sequence'])
 		Seen.append(name)
-		try:
-			lendict[len(name)] = int(lendict[len(name)]) + 1
-		except:
-			lendict[len(name)] = 1
+		leng.append(len(name))
+		# try:
+		# 	lendict[len(name)] = int(lendict[len(name)]) + 1
+		# except:
+		# 	lendict[len(name)] = 1
 	except:
 		pass
 a = {}
 for f in Seen:
 	a[str(f)] = Seen.count(f)
 print(Counter(a).most_common(4))
+a = {}
+for f in leng:
+	a[str(f)] = leng.count(f)
+print(Counter(a).most_common(4))
 # print('files in '+str(i)+' different classes')
-print(lendict)
+# print(lendict)
