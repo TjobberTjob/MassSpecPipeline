f522eb81a7c9b1f3a752e5b3d0e65a2d75eaa07f
TobiaGC <TobiasGChristensen@outlook.dk>
2020-01-07 03:56:59 +0100

    `updated`

--- testest.py
+++ testest.py
@@ -1,5 +1,5 @@
 import json
-from collections import Counter, defaultdict
+from collections import Counter
 import time
 
 with open('config.json') as json_file:
@@ -8,13 +8,16 @@ path = data['path'] + 'metadata/'
 
 # names = [(json.loads(line)['image']+".txt") for line in open(path + 'subimage.json') if 'image' in json.loads(line)]
 Seen = []
-lendict = defaultdict(int)
+lendict = {}
 for line in open(path + 'subimage.json'):
 	try:
 		data = json.loads(line)
 		name = str(data['Sequence'])
 		Seen.append(name)
-		lendict[len(name)] = int(lendict[len(name)]) + 1
+		try:
+			lendict[len(name)] = int(lendict[len(name)]) + 1
+		except:
+			lendict[len(name)] = 1
 	except:
 		pass
 a = {}
@@ -22,4 +25,4 @@ for f in Seen:
 	a[str(f)] = Seen.count(f)
 print(Counter(a).most_common(4))
 # print('files in '+str(i)+' different classes')
-print(lendict)
+# print(lendict)
