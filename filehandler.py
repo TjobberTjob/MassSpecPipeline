b04ce657f207122fd42fd53d0fc052edb7ff464c
TobiaGC <TobiasGChristensen@outlook.dk>
2020-01-07 14:22:43 +0100

    `updated`

--- filehandler.py
+++ filehandler.py
@@ -4,15 +4,15 @@ import sys
 
 
 def filter(path, file):
-    is os.path.exists(path + str(file) + '_filtered.json'):
+    if os.path.exists(path + str(file) + '_filtered.json'):
         os.remove(path + str(file) + '_filtered.json')
 
     # Used to get only most abundant classes
-    Seen = [json.loads(line)['Sequence'] for line in open(path + 'subimage.json') if 'Sequence' in json.loads(line)]
-    a = {}
-    for f in Seen:
-        a[str(f)] = Seen.count(f)
-    Seen = [f[0] for f in Counter(a).most_common(4)]
+    # Seen = [json.loads(line)['Sequence'] for line in open(path + 'subimage.json') if 'Sequence' in json.loads(line)]
+    # a = {}
+    # for f in Seen:
+    #     a[str(f)] = Seen.count(f)
+    # Seen = [f[0] for f in Counter(a).most_common(4)]
 
     lines_seen = set()
     outfile = open(path + str(file) + '_filtered.json', 'w')
@@ -20,7 +20,7 @@ def filter(path, file):
         data = json.loads(line)
 
         # if str(data['size']) == '[166, 66, 4]' and str(data['Sequence']) in Seen:
-        if 'allpeptides' in data and data['allpeptides'] and 'filetypes' in data and 'raw' in data['filetypes']  and line not in lines_seen:  ### FILTER HERE ###
+        if 'allpeptides' in data and data['allpeptides'] and 'filetypes' in data and 'raw' in data['filetypes'] and line not in lines_seen:  ### FILTER HERE ###
             outfile.write(line)
             lines_seen.add(line)
 
