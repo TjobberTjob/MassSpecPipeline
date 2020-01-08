25951ff1f48798492f81545d53b9a8097552d16f
TobiaGC <TobiasGChristensen@outlook.dk>
2020-01-08 13:02:19 +0100

    `updated`

--- testest.py
+++ testest.py
@@ -20,7 +20,7 @@ Leng = np.unique(leng)
 a = {}
 for f in Seen:
 	a[str(f)] = seen.count(f)
-	f"{needle} is out of bounds of {haystack}"
+
 print(f"{Counter(a).most_common(4)}  Total amount of classes: {len(Seen)}")
 a = {}
 for f in Leng:
