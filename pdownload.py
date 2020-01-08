73a62e0b75bdaec2657d44d5cb103149b60a3648
TobiaGC <TobiasGChristensen@outlook.dk>
2020-01-09 00:58:02 +0100

    `updated`

--- pdownload.py
+++ pdownload.py
@@ -501,8 +501,7 @@ if __name__ == '__main__':
     metapath = f'{datapath}metadata/'
 
     try:
-        print(f'rm {datapath}*.*')
-        quit()
+        os.system(f'rm {datapath}*.*')
     except:
         pass
 
