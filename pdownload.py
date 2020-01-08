9a01b8125f60c8f5ce0200ccbeed84bdf5fdb277
TobiaGC <TobiasGChristensen@outlook.dk>
2020-01-09 00:45:10 +0100

    `updated`

--- pdownload.py
+++ pdownload.py
@@ -125,22 +125,18 @@ def formatFile(accnr, filename, path, filepath):
                 pass
             os.system('cd .. && cd ThermoRawFileParser/ && docker build --no-cache -t thermorawparser . && cd '
                       '../MassSpecPipeline/')
-        print(path)
+
         if path[0] == '/':
             relpath = path[:-1]
         else:
             relpath = f'{os.getcwd()}{path[:-1]}'
-        print(relpath)
-        quit()
+
         os.system(f'chmod -R a+rwx {path} *')
         os.system(f'docker run -v "{relpath}:/data_input" -i -t thermorawparser mono '
                   f'bin/x64/Debug/ThermoRawFileParser.exe -i=/data_input/{accnr}/{filename}/file.raw -o=/data_inpu'
                   f't/{accnr}/{filename}/ -f=1 -m=1')
-        # os.system('docker run -v \"' + relpath + ':/data_input\" -i -t thermorawparser mono '
-        #                                          'bin/x64/Debug/ThermoRawFileParser.exe -i=/data_input/' + accnr +
-        #           '/' + filename + '/file.raw -o=/data_input/' + accnr + '/' + filename + '/ -f=1 -m=1')
         os.remove(f'{filepath}file-metadata.txt')
-        # os.remove(f'{filepath}file.raw')
+        os.remove(f'{filepath}file.raw')
 
 
 def process_ms1(spectrum):
