ac7d6049d73300470725e16faae01d0b9972f74b
TobiaGC <TobiasGChristensen@outlook.dk>
2020-01-08 13:28:01 +0100

    `updated`

--- testest.py
+++ testest.py
@@ -4,25 +4,27 @@ from collections import Counter
 import numpy as np
 
 with open('config.json') as json_file:
-	data = json.load(json_file)
+    data = json.load(json_file)
 path = data['path'] + 'metadata/'
 
 # names, length = [(json.loads(line)['image']+".txt") for line in open(path + 'subimage.json') if 'image' in json.loads(line)]
 seen = []
 leng = []
-for line in open(path + 'subimage.json'):
-	data = json.loads(line)
-	if 'Sequence' in data:
-		seen.append(str(data['Sequence']))
-		leng.append(len(data['Sequence']))
+for line in open(f'{path} subimage.json'):
+    data = json.loads(line)
+    if 'Sequence' in data:
+        seen.append(str(data['Sequence']))
+        leng.append(len(data['Sequence']))
 Seen = np.unique(seen)
 Leng = np.unique(leng)
 a = {}
 for f in Seen:
-	a[str(f)] = seen.count(f)
+    a[str(f)] = seen.count(f)
 
 print(f"{Counter(a).most_common(4)}  Total amount of classes: {len(Seen)}")
 a = {}
 for f in Leng:
-	a[str(f)] = leng.count(f)
+    a[str(f)] = leng.count(f)
 print(Counter(a).most_common(4))
+
+# find /data/ProteomeToolsRaw/ -name file.mzML -exec rm -f {} \;
