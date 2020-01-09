bd9d14895cfa3daf82c6e7ae719351e864894202
TobiaGC <TobiasGChristensen@outlook.dk>
2020-01-09 01:52:32 +0100

    Updates this bit

--- pdownload.py
+++ pdownload.py
@@ -123,11 +123,11 @@ def formatFile(accnr, filename, path, filepath):
         else:
             relpath = f'{os.getcwd()}{path[:-1]}'
 
-        print(f'chmod -R a+rwx {path} *')
+        os.system(f'chmod -R a+rwx {path}*')
         os.system(f'docker run -v "{relpath}:/data_input" -i -t thermorawparser mono '
               f'bin/x64/Debug/ThermoRawFileParser.exe -i=/data_input/{accnr}/{filename}/file.raw -o=/data_inpu'
               f't/{accnr}/{filename}/ -f=1 -m=1')
-        quit()
+
         os.remove(f'{filepath}file-metadata.txt')
         os.remove(f'{filepath}file.raw')
 
