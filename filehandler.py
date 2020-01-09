816e38334244e3dd18c05160604adb8090814574
TobiaGC <TobiasGChristensen@outlook.dk>
2020-01-09 02:15:49 +0100

    Updates this bit

--- filehandler.py
+++ filehandler.py
@@ -20,7 +20,7 @@ def filter(path, file):
     for line in open(f'{path}{str(file)}.json', 'r'):
         data = json.loads(line)
 
-        if str(data['size']) == '[166, 66, 4]' and str(data['Sequence']) in Seen:
+        if str(data['size']) == '[166, 66, 4]' and str(data['Sequence']) in line:
             data['seqlen'] = len(data['Sequence'])
             outfile.write(json.dumps(data) + '\n')
 
