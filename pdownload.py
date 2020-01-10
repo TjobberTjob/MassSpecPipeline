677f4732e1a5faa5ee53a50ce20d55c3073f2523
TobiaGC <TobiasGChristensen@outlook.dk>
2020-01-10 12:58:42 +0100

    Updates this bit

--- pdownload.py
+++ pdownload.py
@@ -42,16 +42,13 @@ def filefinder(accnr, path):
     if not os.path.exists(f'{path}{accnr}/'):
         os.mkdir(f'{path}{accnr}/')
 
-    haveallMQF = True
+
     allCheck = ['allPeptides.txt' in os.listdir(f'{path}{accnr}/{files}/') for files in os.listdir(f'{path}{accnr}/') if len(os.listdir(f'{path}{accnr}/')) == len(rawfiles)]
-    print(allCheck)
-    for files in os.listdir(f'{path}{accnr}/'):
-        if not 'allPeptides.txt' in os.listdir(f'{path}{accnr}/{files}') or len(os.listdir(f'{path}{accnr}/')) != len(rawfiles):
-            haveallMQF = False
-            # print('hey1')
-            break
-            # print('hey2')
-    quit()
+    if False in allCheck:
+        haveallMQF = False
+    else:
+        haveallMQF = True
+    print(haveallMQF)
 
     return zipfiles, rawfiles, haveallMQF
 
