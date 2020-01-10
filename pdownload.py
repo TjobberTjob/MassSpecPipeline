0eda600b07d4a562d9feb61b92581845a1e15d02
TobiaGC <TobiasGChristensen@outlook.dk>
2020-01-10 12:43:12 +0100

    Updates this bit

--- pdownload.py
+++ pdownload.py
@@ -44,7 +44,7 @@ def filefinder(accnr, path):
 
     haveallMQF = True
     for files in os.listdir(f'{path}{accnr}/'):
-        if not 'allPeptides.txt' in os.listdir(f'{path}{accnr}/{files}') or len(os.listdir(f'{path}{accnr}/')) != len(rawfiles):
+        if (not 'allPeptides.txt' in os.listdir(f'{path}{accnr}/{files}')) or len(os.listdir(f'{path}{accnr}/')) != len(rawfiles):
             haveallMQF = False
             break
     print(len(os.listdir(f'{path}{accnr}/')) != len(rawfiles))
