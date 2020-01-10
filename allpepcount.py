b06a3542a780f5887a867aab3e5bc9ca08f38137
TobiaGC <TobiasGChristensen@outlook.dk>
2020-01-10 02:33:42 +0100

    Updates this bit

--- allpepcount.py
+++ allpepcount.py
@@ -6,6 +6,6 @@ i = 0
 for line in open(path + 'subimage_filtered.json'):
     data = json.loads(line)
     if 'Length' in data and data['Length'] == 11:
-            i += 1
+        i += 1
 print(i)
 # print(accessions)
