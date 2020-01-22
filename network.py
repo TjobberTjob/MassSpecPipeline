9b7e868227b6fc8d93a8d36e915d08b46bf11a95
TobiaGC <TobiasGChristensen@outlook.dk>
2020-01-22 22:32:23 +0100

    Updates this bit

--- network.py
+++ network.py
@@ -42,8 +42,7 @@ def datafetcher(path, imgpath, classification, imageclass, splitratio, test_acce
                       'accession' in json.loads(line)]
         random.shuffle(accessions)
         testlist = [f'{json.loads(line)["image"]}.txt' for line in open(f'{path}subimage_filtered.json') if
-                    json.loads(line)['accession'] in accessions[:test_accessions]]
-        print(testlist)
+                    json.loads(line)['accession'] in accessions[0]]
 
         labels = {}
         testlabels = {}
