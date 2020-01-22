ad80db9a0490f5163cab0446c325ee2be6920060
TobiaGC <TobiasGChristensen@outlook.dk>
2020-01-22 20:51:52 +0100

    Updates this bit

--- network.py
+++ network.py
@@ -53,7 +53,7 @@ def datafetcher(path, imgpath, classification, imageclass, splitratio):
         test_list = []
         for line in open(f'{path}subimage_filtered.json'):
             dat = json.loads(line)
-            if 'accessions' in dat and dat['accession'] in accessions:
+            if dat['accession'] in accessions:
                 test_list.append(f'{json.loads(line)["image"]}.txt')
         print(test_list)
         quit()
