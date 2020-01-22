140d5e410271474a611ade03ec36779dcec408c5
TobiaGC <TobiasGChristensen@outlook.dk>
2020-01-22 17:46:21 +0100

    Updates this bit

--- filehandler.py
+++ filehandler.py
@@ -40,7 +40,7 @@ def filter(path, file):
     # lines_seen = set()
     outfile = open(f'{path}{str(file)}_filtered.json', 'w')
     scores = [json.loads(line)['PIF'] for line in open(f'{path}{str(file)}.json') if 'PIF' in json.loads(line)]
-    print(sum(scores) / len(scores))
+    print(sum(float(scores)) / len(scores))
     quit()
     for line in open(f'{path}{str(file)}.json', 'r'):
         data = json.loads(line)
