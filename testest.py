6a595baeee876d6554270477e77e18151a4b4000
TobiaGC <TobiasGChristensen@outlook.dk>
2020-01-22 20:00:08 +0100

    Updates this bit

--- testest.py
+++ testest.py
@@ -1,43 +1,38 @@
 import json
+import os
 from collections import Counter
 from statistics import mean
 
 import numpy as np
 
-with open('config.json') as json_file:
-    data = json.load(json_file)
-path = f'{data["path"]}metadata/'
-
-# scores = []
+# with open('config.json') as json_file:
+#     data = json.load(json_file)
+# path = f'{data["path"]}metadata/'
+#
+# seen = []
+# leng = []
 # for line in open(f'{path}subimage.json'):
-#     try:
-#         if 'PIF' in json.loads(line):
-#             scores.append(float(json.loads(line)['PIF']))
-#     except:
-#         pass
-# meanpif = np.percentile(intlist, 0.5)
+#     data = json.loads(line)
+#     if 'Sequence' in data:# and 'PIF' in data and data['PIF'] != 'Non Numérique' and float(data['PIF']) > 0.25:
+#         seen.append(str(data['Sequence']))
+#         leng.append(len(data['Sequence']))
+#
+# Seen = np.unique(seen)
+# Leng = np.unique(leng)
+#
+# a = {}
+# for f in Seen:
+#     a[str(f)] = seen.count(f)
+# print(f"{Counter(a).most_common(10)}  Total amount of classes: {len(Seen)}")
+#
+# a = {}
+# for f in Leng:
+#     a[str(f)] = leng.count(f)
+# print(Counter(a).most_common(10))
 
-seen = []
-leng = []
-for line in open(f'{path}subimage.json'):
-    data = json.loads(line)
-    if 'Sequence' in data:# and 'PIF' in data and data['PIF'] != 'Non Numérique' and float(data['PIF']) > 0.25:
-        seen.append(str(data['Sequence']))
-        leng.append(len(data['Sequence']))
-
-Seen = np.unique(seen)
-Leng = np.unique(leng)
-
-a = {}
-for f in Seen:
-    a[str(f)] = seen.count(f)
-print(f"{Counter(a).most_common(10)}  Total amount of classes: {len(Seen)}")
-
-a = {}
-for f in Leng:
-    a[str(f)] = leng.count(f)
-print(Counter(a).most_common(10))
-
+listofshit = os.listdir('/data/ProteomeToolsRaw/*/*')
+print(listofshit)
+quit()
 
 # find /data/ProteomeToolsRaw/ -name file.mzML -exec rm -f {} \;
 # find /data/ProteomeToolsRaw/ -name file-metadata.txt -exec rm -f {} \;
