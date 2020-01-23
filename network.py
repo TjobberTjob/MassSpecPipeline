115b80f157663d54b1c9e75aefefef427834ff95
TobiaGC <TobiasGChristensen@outlook.dk>
2020-01-23 22:24:25 +0100

    Updates this bit

--- network.py
+++ network.py
@@ -229,16 +229,19 @@ if __name__ == '__main__':
     if sys.argv[4] == 'test':
         imageclass = sys.argv[2]
         model = load_model(f'{metapath}Best-{imageclass}.h5')
-        test_generator = DataGenerator(imagepath, partition['test'], testlabels, **params)
-        testaccuracy = model.evaluate_generator(test_generator)
-        print(f'Accuracy on test data. Loss: {testaccuracy[0]}. Accuracy: {testaccuracy[1]}')
-        quit()
+        print(model.summary())
+        # test_generator = DataGenerator(imagepath, partition['test'], testlabels, **params)
+        # testaccuracy = model.evaluate_generator(test_generator)
+        # print(f'Accuracy on test data. Loss: {testaccuracy[0]}. Accuracy: {testaccuracy[1]}')
+        # quit()
 
     training_generator = DataGenerator(imagepath, partition['train'], labels, **params)
     validation_generator = DataGenerator(imagepath, partition['validation'], labels, **params)
 
     output = nnmodel(imglen, pixellen, classification, n_channels, n_classes, nameofclass, metapath)
     model = output[0]
+    print(model.summary())
+    quit()
     callbacks_list = output[1]
     history = model.fit_generator(generator=training_generator, validation_data=validation_generator, epochs=epochs, callbacks=callbacks_list)
     if classification:
