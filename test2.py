a92720f320e37ee1861e3c96e741a4d06fd761aa
TobiaGC <TobiasGChristensen@outlook.dk>
2020-01-24 23:26:21 +0100

    Updates this bit

--- test2.py
+++ test2.py
@@ -7,6 +7,6 @@ for f in os.listdir('/data/ProteomeToolsRaw/'):
     i += 1
     if os.path.isdir(f'/data/ProteomeToolsRaw/{f}') and f[0:3] == 'PXD' or f[0:3] == 'PRD':
         for g in os.listdir(f'/data/ProteomeToolsRaw/{f}'):
-            if g == 'ECS_A6':
+            if g == 'Animal_20150123_ASK_RAN_TAM_C':
                 print(f'{i} / {str(len(os.listdir("/data/ProteomeToolsRaw/")))}')
 
