11dd99056d9a014abc987684c1f91f3c17b4dbcc
TobiaGC <TobiasGChristensen@outlook.dk>
2020-01-24 03:06:05 +0100

    Updates this bit

--- test.py
+++ test.py
@@ -4,6 +4,6 @@ import os
 
 for f in os.listdir('/data/ProteomeToolsRaw/'):
     print(f)
-    if os.path.isdir(f) and f[0:3] == 'PXD' or f[0:3] == 'PRD':
+    if os.path.isdir(f'/data/ProteomeToolsRaw/{f}') and f[0:3] == 'PXD' or f[0:3] == 'PRD':
         print(f)
         quit()
