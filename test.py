75312911affafdfc3ae369610663f794d704c501
TobiaGC <TobiasGChristensen@outlook.dk>
2020-01-24 03:05:49 +0100

    Updates this bit

--- test.py
+++ test.py
@@ -3,6 +3,7 @@ import json
 import os
 
 for f in os.listdir('/data/ProteomeToolsRaw/'):
+    print(f)
     if os.path.isdir(f) and f[0:3] == 'PXD' or f[0:3] == 'PRD':
         print(f)
         quit()
