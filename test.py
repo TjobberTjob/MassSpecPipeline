d1fc8665926d06beb0364c8568d2a74298339201
TobiaGC <TobiasGChristensen@outlook.dk>
2020-01-24 02:55:17 +0100

    Updates this bit

--- test.py
+++ test.py
@@ -1,7 +1,13 @@
 import gzip
 import json
 
-for line in open(f'/data/ProteomeToolsRaw/PXD010595/02101a_GA8-TUM_proteo_TMT_8_01_01-ETD-1h-R1/mzML.json', 'r'):
-    data = json.loads(line)
-    with gzip.GzipFile('/data/ProteomeToolsRaw/PXD010595/02101a_GA8-TUM_proteo_TMT_8_01_01-ETD-1h-R1/mzML2.json', 'w') as fout:
-        fout.write(json.dumps(data).encode('utf-8'))
+# for line in open(f'/data/ProteomeToolsRaw/PXD010595/02101a_GA8-TUM_proteo_TMT_8_01_01-ETD-1h-R1/mzML.json', 'r'):
+#     data = json.loads(line)
+#     with gzip.GzipFile('/data/ProteomeToolsRaw/PXD010595/02101a_GA8-TUM_proteo_TMT_8_01_01-ETD-1h-R1/mzML2.json', 'w') as fout:
+#         fout.write(json.dumps(data).encode('utf-8'))
+i = 0
+while i < 10:
+    with gzip.GzipFile('/data/ProteomeToolsRaw/PXD010595/02101a_GA8-TUM_proteo_TMT_8_01_01-ETD-1h-R1/mzML2.json', 'r') as fin:
+        data = json.loads(fin.read().decode('utf-8'))
+    print(data)
+    i+=1
