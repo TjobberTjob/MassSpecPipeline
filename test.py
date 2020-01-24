8c96d384dda39737038bae3291bb355935af361a
TobiaGC <TobiasGChristensen@outlook.dk>
2020-01-24 03:15:02 +0100

    Updates this bit

--- test.py
+++ test.py
@@ -6,13 +6,15 @@ for f in os.listdir('/data/ProteomeToolsRaw/'):
     if os.path.isdir(f'/data/ProteomeToolsRaw/{f}') and f[0:3] == 'PXD' or f[0:3] == 'PRD':
         print(f'/data/ProteomeToolsRaw/{f}')
         for g in os.listdir(f'/data/ProteomeToolsRaw/{f}'):
-            try:
-                path = f'/data/ProteomeToolsRaw/{f}/{g}/'
-                print(path)
-                data = json.load(open(f'{path}mzML.json'))
-
-                with gzip.GzipFile(f'{path}mzML2.json', 'w') as fout:
-                    fout.write(json.dumps(data).encode('utf-8'))
-                quit()
-            except:
-                pass
+            print(g)
+        quit()
+            # try:
+            #     path = f'/data/ProteomeToolsRaw/{f}/{g}/'
+            #     print(path)
+            #     data = json.load(open(f'{path}mzML.json'))
+            #
+            #     with gzip.GzipFile(f'{path}mzML2.json', 'w') as fout:
+            #         fout.write(json.dumps(data).encode('utf-8'))
+            #     quit()
+            # except:
+            #     pass
