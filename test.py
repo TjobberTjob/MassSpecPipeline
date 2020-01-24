3e488a1a4f4d05498473c33a8c22286e35a768b3
TobiaGC <TobiasGChristensen@outlook.dk>
2020-01-24 02:50:43 +0100

    Updates this bit

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import gzip
 import json
 
-for line in open(f'{path}{str(file)}.json', 'r'):
+for line in open(f'/data/ProteomeToolsRaw/PXD010595/02101a_GA8-TUM_proteo_TMT_8_01_01-ETD-1h-R1/mzML.json', 'r'):
     data = json.loads(line)
     with gzip.GzipFile('/data/ProteomeToolsRaw/PXD010595/02101a_GA8-TUM_proteo_TMT_8_01_01-ETD-1h-R1/mzML2.json', 'w') as fout:
         fout.write(json.dumps(data).encode('utf-8'))
