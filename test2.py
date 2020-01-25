7881901da99bc8d5c08dab5d56856231a8a083d9
TobiaGC <TobiasGChristensen@outlook.dk>
2020-01-25 21:57:24 +0100

    Updates this bit

--- test2.py
+++ test2.py
@@ -3,12 +3,10 @@ import json
 import os
 i = 0
 a = [f'/data/ProteomeToolsRaw/{f}/{g}/allPeptides.txt' for f in os.listdir('/data/ProteomeToolsRaw/') for g in os.listdir(f'/data/ProteomeToolsRaw/{f}') if os.path.exists(f'/data/ProteomeToolsRaw/{f}/{g}/allPeptides.txt')]
-print(len(a))
-quit()
 for f in os.listdir('/data/ProteomeToolsRaw/'):
-    i += 1
     if os.path.isdir(f'/data/ProteomeToolsRaw/{f}') and f[0:3] == 'PXD' or f[0:3] == 'PRD':
         for g in os.listdir(f'/data/ProteomeToolsRaw/{f}'):
+            i += 1
             if g == '20160704_UCL_Mouse_IgG_30min_2a':
-                print(f'{i} / {str(len(os.listdir("/data/ProteomeToolsRaw/")))}')
+                print(f'{i} / {str(len(a))}')
 
