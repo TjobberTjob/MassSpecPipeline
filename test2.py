a134c6fb711cf0539b5c1ca1641dd9659c931646
TobiaGC <TobiasGChristensen@outlook.dk>
2020-01-25 23:29:51 +0100

    Updates this bit

--- test2.py
+++ test2.py
@@ -7,6 +7,6 @@ for f in os.listdir('/data/ProteomeToolsRaw/'):
     if os.path.isdir(f'/data/ProteomeToolsRaw/{f}') and f[0:3] == 'PXD' or f[0:3] == 'PRD':
         for g in os.listdir(f'/data/ProteomeToolsRaw/{f}'):
             i += 1
-            if g == '20160704_UCL_Mouse_IgG_30min_2a':
+            if g == 'tryp_02h_5':
                 print(f'{i} / {str(len(a))}')
 
