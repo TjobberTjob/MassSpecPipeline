aa5673e1880ddc560733f2401a994330aa3f8315
TobiaGC <TobiasGChristensen@outlook.dk>
2020-01-26 02:40:10 +0100

    Updates this bit

--- test2.py
+++ test2.py
@@ -7,6 +7,6 @@ for f in os.listdir('/data/ProteomeToolsRaw/'):
     if os.path.isdir(f'/data/ProteomeToolsRaw/{f}') and f[0:3] == 'PXD' or f[0:3] == 'PRD':
         for g in os.listdir(f'/data/ProteomeToolsRaw/{f}'):
             i += 1
-            if g == 'QEx1_002014':
+            if g == 'QEXI22259':
                 print(f'{i} / {str(len(a))}')
 
