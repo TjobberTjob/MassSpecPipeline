324cd9145627f78580ee54cbabb18050e6b4e64f
TobiaGC <TobiasGChristensen@outlook.dk>
2020-01-30 19:55:29 +0100

    Updates this bit

--- extractor.py
+++ extractor.py
@@ -578,12 +578,12 @@ if __name__ == '__main__':
                 debuggerlist = [accession, 'No error']
                 debuggerFile.write(f'{debuggerlist}\n')
                 debuggerFile.close()
-            except:# Exception as e:
-                # print(f'Problem occured with: {accession}. unable to proceed at this time')
-                # os.system(f'rm {datapath}*.*')
-                # debuggerlist = [accession, e]
-                # debuggerFile.write(f'{debuggerlist}\n')
-                # debuggerFile.close()
+            except Exception as e:
+                print(f'Problem occured with: {accession}. unable to proceed at this time')
+                os.system(f'rm {datapath}*.*')
+                debuggerlist = [accession, e]
+                debuggerFile.write(f'{debuggerlist}\n')
+                debuggerFile.close()
                 pass
 
     else:
