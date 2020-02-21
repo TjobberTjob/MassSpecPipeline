bac0866f8200c59989ca85bb06a85bbc1675d23b
TobiaGC <TobiasGChristensen@outlook.dk>
2020-02-21 13:51:58 +0100

    Updated model

--- extractor.py
+++ extractor.py
@@ -145,12 +145,13 @@ def formatFile(accnr, filename, path, filepath):
             relpath = f'{os.getcwd()}{path[:-1]}'  # Either gives path as root path or have data as a sub folder to the one the code is in
 
         os.system(f'chmod -R a+rwx {path}*')
-        try:
+        if os.path.exists(f'{filepath}file.raw'):
             os.system(f'docker run -v "{relpath}:/data_input" -i -t thermorawparser mono '
                   f'bin/x64/Debug/ThermoRawFileParser.exe -i=/data_input/{accnr}/{filename}/file.raw -o=/data_inpu'
                   f't/{accnr}/{filename}/ -f=1 -m=1')
-        except:
-            print('Problem formatting file, raw file issues')
+        else:
+            print('No raw file, cannot format')
+            return
 
         os.remove(f'{filepath}file-metadata.txt')
         os.remove(f'{filepath}file.raw')
