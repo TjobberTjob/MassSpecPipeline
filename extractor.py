1d5f00b664599650e4de698b3f983d9405c3a482
TobiaGC <TobiasGChristensen@outlook.dk>
2020-02-24 20:18:39 +0100

    Updated model

--- extractor.py
+++ extractor.py
@@ -591,7 +591,7 @@ def partOne(accnr, maxquant_file, path, mpath):
 
             for things in os.listdir(f'{path}'):
                 if things.endswith('.txt') or things.endswith('.zip'):
-                    print(f'{path}{things}')
+                    os.remove(f'{path}{things}')
 
             brokenfiles.append(zips.replace(' ', '%20'))
             pass
