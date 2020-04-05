e31bc6df06289178b6a619f6acf11b17bb8a4dff
TjobberTjob <tobiasgc95@hotmail.com>
2020-04-06 00:12:16 +0200

    changed config.json

--- network.py
+++ network.py
@@ -237,8 +237,8 @@ if __name__ == '__main__':
         plt.legend(['train', 'validation'], loc='upper left')
         plt.savefig(f'{metapath}{imageclass}.png')
     else:
-        plt.plot(history.history['r2'])
-        plt.plot(history.history['val_r2'])
+        plt.plot(history.history[r2])
+        plt.plot(history.history[val_r2])
         plt.title('R-squared of training')
         plt.ylabel('r^2')
         plt.xlabel('epoch')
