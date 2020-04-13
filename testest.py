c969f716836ea1a9113087bc587ff796a73cdc8d
TjobberTjob <tobiasgc95@hotmail.com>
2020-04-14 00:05:24 +0200

    changed config.json

--- testest.py
+++ testest.py
@@ -6,30 +6,38 @@ from glob import glob
 from statistics import mean
 import numpy as np
 
+
+
 with open('config.json') as json_file:
     data = json.load(json_file)
 path = f'{data["path"]}metadata/'
-
-seen = []
-leng = []
-for line in open(f'{path}subimage.json'):
+i = 0
+for line in open(f'{path}accessions.json'):
     data = json.loads(line)
-    if 'Sequence' in data:# and 'PIF' in data and data['PIF'] != 'Non Numérique' and float(data['PIF']) > 0.25:
-        seen.append(str(data['Sequence']))
-        leng.append(len(data['Sequence']))
-
-Seen = np.unique(seen)
-Leng = np.unique(leng)
-
-a = {}
-for f in Seen:
-    a[str(f)] = seen.count(f)
-print(f"{Counter(a).most_common(10)}  Total amount of classes: {len(Seen)}")
-
-a = {}
-for f in Leng:
-    a[str(f)] = leng.count(f)
-print(f'{Counter(a).most_common(10)} Total images: {len(os.listdir(f"{path[:-9]}images/"))}')
+    if data['maxquant']:
+        i += 1
+print(i)
+#
+# seen = []
+# leng = []
+# for line in open(f'{path}subimage.json'):
+#     data = json.loads(line)
+#     if 'Sequence' in data:# and 'PIF' in data and data['PIF'] != 'Non Numérique' and float(data['PIF']) > 0.25:
+#         seen.append(str(data['Sequence']))
+#         leng.append(len(data['Sequence']))
+#
+# Seen = np.unique(seen)
+# Leng = np.unique(leng)
+#
+# a = {}
+# for f in Seen:
+#     a[str(f)] = seen.count(f)
+# print(f"{Counter(a).most_common(10)}  Total amount of classes: {len(Seen)}")
+#
+# a = {}
+# for f in Leng:
+#     a[str(f)] = leng.count(f)
+# print(f'{Counter(a).most_common(10)} Total images: {len(os.listdir(f"{path[:-9]}images/"))}')
 
 
 # find /data/ProteomeToolsRaw/ -name file.mzML -exec rm -f {} \;
