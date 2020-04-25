78a0c5d1cd937e8db7ce470b146dc07f8fe16943
TjobberTjob <tobiasgc95@hotmail.com>
2020-04-26 01:17:14 +0200

    changed config.json

--- filter.py
+++ filter.py
@@ -64,8 +64,15 @@ def filter(path, file):
                 ms1size = sizes[0]
                 break
         start = time.time()
-        getscores = [float(lines[re.search('score', lines.lower()).span()[1] + 4: min(f for f in [m.start() for m in re.finditer(',', lines.lower())] if f > re.search('score', lines.lower()).span()[1] + 4) - 1])
-                     for lines in open(f'{path}subimage.json') if 'score' in lines.lower()]
+
+        getscores = []
+        for lines in open(f'{path}subimage.json'):
+            if 'score' in lines.lower():
+                pointA = re.search('score', lines.lower()).span()[1] + 4
+                pointB = min(f for f in [m.start() for m in re.finditer(',', lines.lower())] if f > pointA - 1)
+                getscores.append(float(lines[pointA: pointB]))
+        # getscores = [float(lines[re.search('score', lines.lower()).span()[1] + 4: min(f for f in [m.start() for m in re.finditer(',', lines.lower())] if f > re.search('score', lines.lower()).span()[1] + 4) - 1])
+        #              for lines in open(f'{path}subimage.json') if 'score' in lines.lower()]
         end = time.time()
         print(end-start)
         getabovehere = np.percentile(getscores, 60)
