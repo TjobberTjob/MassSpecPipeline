bf75dd3c71301d9cacf35fed7e6531e63d207463
TjobberTjob <tobiasgc95@hotmail.com>
2020-04-26 17:05:52 +0200

    changed config.json

--- filter.py
+++ filter.py
@@ -169,6 +169,7 @@ def filtercharge(path, outfile, getabovehere, ms1size):
             outfile.write(line + '\n')
             namesseen.append(name)
             i += 1
+            print(i)
 
     outfile.close()
     print(f'Length of filtered file: {i}')
