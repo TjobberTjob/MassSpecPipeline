9e01fe301ee149724a02bd5961ff22200eb5ad2c
TjobberTjob <tobiasgc95@hotmail.com>
2020-04-26 20:19:04 +0200

    changed config.json

--- filter.py
+++ filter.py
@@ -130,7 +130,7 @@ def filtercharge(path, outfile, getabovehere, ms1size):
         name = line[re.search('image', line).span()[1]+4: min(f for f in re.search(',', line).span() if f > re.search('image', line).span()[1]+3) -1]
         # if '"Charge"' in line:
             # charge = line[re.search('Charge', line).span()[1]+3: min(f for f in re.search(',', line).span() if f > re.search('Charge', line).span()[1]+3)]
-        print(name, charge)
+        print(name)
         quit()
         for f in a:
             if len(checklist) == 4:
