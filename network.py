948bbc5145dbd7eb2e684aa76e783af0a00af466
TjobberTjob <tobiasgc95@hotmail.com>
2020-04-27 14:45:52 +0200

    Working on optizing json retrieval

--- network.py
+++ network.py
@@ -105,20 +105,48 @@ def datafetcher(path, imgpath, imageclass, test_accessions, whichMS):
     for f in testfiles:
         tests[f.split('-')[-1][:-5]].append(f)
 
+    start = time.time()
+    labels = {}
+    testlabels = {}
+    for line in open(f'{path}{filetosuse}'):
+        data = loads(line)
+        name = data['images']
+        if name not in tests[name.split('-')[-1][:-5]]:
+            labels[name] = data[imageclass]
+        else:
+            labels[name] = data[imageclass]
+    stop = time.time()
+    print(stop - start)
+
+    start = time.time()
     labels = {}
     testlabels = {}
     for line in open(f'{path}{filetosuse}'):
         name = line.split(', "')[0][11:-1]
-        print(name,name.split('-')[-1][:-5],tests[name.split('-')[-1][:-5]])
-        quit()
+        label = getclass(f'{str(imageclass)}', line)
+        if name not in tests[name.split('-')[-1][:-5]]:
+            labels[name] = label
+        else:
+            testlabels[name] = label
+    stop = time.time()
+    print(stop - start)
+
+    start = time.time()
+    labels = {}
+    testlabels = {}
+    for line in open(f'{path}{filetosuse}'):
+        name = line.split(', "')[0][11:-1]
         if name not in tests[name.split('-')[-1][:-5]]:
             getlabel = [f for f in [m.start() for m in re.finditer('"', line)] if f > re.search(f'{str(imageclass)}', line).span()[1]]
             label = line[getlabel[0] + 1: getlabel[1]]
             labels[name] = label
         else:
-            getlabel = [f for f in [m.start() for m in re.finditer('"', line)] if f > re.search(f'{str(imageclass)}', line).span()[1]]
+            getlabel = [f for f in [m.start() for m in re.finditer('"', line)] if
+                        f > re.search(f'{str(imageclass)}', line).span()[1]]
             label = line[getlabel[0] + 1: getlabel[1]]
             testlabels[name] = label
+    stop = time.time()
+    print(stop - start)
 
     return partition, labels, imagelen, pixellen, testlabels
 
