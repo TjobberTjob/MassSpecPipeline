cd99e24b8587a1e8692dd8d7df95e5eb1340fb55
TjobberTjob <tobiasgc95@hotmail.com>
2020-04-27 10:28:40 +0200

    changed config.json

--- network.py
+++ network.py
@@ -49,7 +49,8 @@ def datafetcher(path, imgpath, imageclass, test_accessions, whichMS):
     elif os.path.exists(f'{path}subimage_filtered.json'):
         filetouse = 'subimage_filtered.json'
     else:
-        filetouse = 'subimage.json
+        filetouse = 'subimage.json'
+
     for singleline in open(f'{path}{filetouse}', 'r'):
         print(singleline)
         imgname = singleline.split(', "')[0][11:-1]
