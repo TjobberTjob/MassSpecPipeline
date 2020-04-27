d6135f89cba0c733f79c8f16ac1398a68ca6618e
TjobberTjob <tobiasgc95@hotmail.com>
2020-04-27 13:15:30 +0200

    Working on optizing json retrieval

--- network.py
+++ network.py
@@ -61,6 +61,12 @@ def datafetcher(path, imgpath, imageclass, test_accessions, whichMS):
     pixellen = len(image[0])
 
     start = time.time()
+    for f in open(f'{path}{filetouse}'):
+        try:
+            data = loads(f)
+        except:
+            print(f)
+            quit()
     accs = [loads(acc)['accession'] for acc in open(f'{path}{filetouse}') if 'accession' in acc]
     end = time.time()
     print(end-start)
