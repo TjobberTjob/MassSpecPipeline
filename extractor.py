9fafc18fa9fab65bb09218d949ff947fd2824b76
TjobberTjob <tobiasgc95@hotmail.com>
2020-04-30 22:16:31 +0200

    optimizing image creations

--- extractor.py
+++ extractor.py
@@ -489,14 +489,14 @@ def subimgs(accnr, interval, bins, image, bounds, resolution, mzmlfile, path, mp
         try:
             ms2info = [mzmlfile['ms2'][ms2scan]['m/z_array'],
                        [math.log(intval) for intval in mzmlfile['ms2'][ms2scan]['rt_array']]]
-            # ms2info = [[mz, int] for mz in ms2info[0] for int in ms2info[1]]
+            ms2info = [[mz, int] for mz in ms2info[0] for int in ms2info[1]]
             datacollected = 'both'
         except:
             ms2info = []
             datacollected = 'ms1'
 
-        ms2size = np.array(ms2info).shape
-        # ms2size = str([f for f in np.array(ms2info).shape])
+        # ms2size = np.array(ms2info).shape
+        ms2size = str([f for f in np.array(ms2info).shape])
         fullsubimage = {'ms1': subimage, 'ms2': ms2info}
 
         # Save image as json file
